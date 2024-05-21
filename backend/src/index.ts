a06739239c3a851844da2b5867c672ab74a849e1
SAI LALITH KUMAR RAJU K <100327503+PhoenixRiseA@users.noreply.github.com>
2024-05-21 13:25:46 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,5 +17,3 @@ app.route("/api/v1/blog", blogRouter);
 
 export default app;
 
-// DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiYjA1N2YwN2QtODE5Zi00ZTFlLTg5YmEtZTExMTU3YmJiYmM0IiwidGVuYW50X2lkIjoiMzJmODVmOWE5ZTI4YWI4YjVjMjEyYTI0ZTY5ZTRjZjRjN2FmYTRmMjFiZGRlYTA4ZjFlNThhYWFjMmY5ZGVmMiIsImludGVybmFsX3NlY3JldCI6IjNhZmUzNjIzLTQ0Y2ItNDA3MS05NzIyLTkyOGNjMDQ4OTc3MSJ9.g2Z6uomAvfKLbPU7rPLsU0K2_JYrr3paseBOf4Wj-60"
-// postgresql://sailalith1993:BhwATX0ym9ku@ep-odd-river-38558734.us-east-2.aws.neon.tech/neondb?sslmode=require
